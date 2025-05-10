2ff2773b5a66f428b36e1697fed88e14911bfc27
Gustavo Malisse <gostavomalisse@gmail.com>
2025-05-10 09:54:19 -0300

    Created the models for each class.

---
+++ VehiclesApp/Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace VehiclesApp.Models
+{
+    class Vehicle
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int ManufacturingDate { get; set; }
+        public required int ModelDate { get; set; }
+        public required string Brand { get; set; } // Posteriormente puxar da tabela das marcas como lista suspensa
+        public string? Observation { get; set; }
+    }
+}
